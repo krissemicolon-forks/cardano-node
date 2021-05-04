60badc8a00ac75a31543378a5b06f9a430c9a862
Kosyrev Serge <serge.kosyrev@iohk.io>
2021-05-04 18:05:46 +0300

    CAD-2907 stub compute:  calibrate at startup, with configurable targets

--- cardano-node/src/Cardano/Node/Protocol/Cardano.hs
+++ cardano-node/src/Cardano/Node/Protocol/Cardano.hs
@@ -16,6 +16,7 @@ module Cardano.Node.Protocol.Cardano
 
 import           Prelude
 
+import           Control.Monad.IO.Class (liftIO)
 import           Control.Monad.Trans.Except (ExceptT)
 import           Control.Monad.Trans.Except.Extra (firstExceptT)
 import qualified Data.Text as T
@@ -28,6 +29,7 @@ import qualified Ouroboros.Consensus.Cardano.CanHardFork as Consensus
 import           Ouroboros.Consensus.HardFork.Combinator.Condense ()
 
 import           Ouroboros.Consensus.Cardano.Condense ()
+import           Ouroboros.Consensus.Shelley.Ledger.Stub
 
 import           Cardano.Api.Orphans ()
 import           Cardano.Api.Protocol.Types
@@ -78,7 +80,9 @@ mkSomeConsensusProtocolCardano NodeByronProtocolConfiguration {
                            }
                            NodeShelleyProtocolConfiguration {
                              npcShelleyGenesisFile,
-                             npcShelleyGenesisFileHash
+                             npcShelleyGenesisFileHash,
+                             npcStubComputeDuration,
+                             npcStubComputePrecision
                            }
                            NodeHardForkProtocolConfiguration {
                              npcTestEnableDevelopmentHardForkEras,
@@ -92,6 +96,11 @@ mkSomeConsensusProtocolCardano NodeByronProtocolConfiguration {
                              npcTestAlonzoHardForkAtVersion
                            }
                            files = do
+    liftIO $
+      calibrateStubComputationArgForTime
+        npcStubComputeDuration
+        npcStubComputePrecision
+
     byronGenesis <-
       firstExceptT CardanoProtocolInstantiationErrorByron $
         Byron.readGenesis npcByronGenesisFile
